70a3bdf8aeef89ab159dc5830a48c437ab558918
Kirill Firsov <kifirigor@gmail.com>
2019-02-28 17:26:35 +0300

    Add files via upload

---
+++ hometask5/src/main/java/ru/firsov/entity/Product.java
@@ -0,0 +1,25 @@
+package ru.firsov.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "cost")
+    private double cost;
+}
